f1b02fe3d93f33f46df9eaf4ba16530f930f9c4d
Noureddine <nourredinesellami@hotmail.com>
2022-10-25 13:32:54 +0200

    create interface Fly and Eagle which implement Fly

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,30 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.printf("%s takes off in the sky.%n", getName());        
+    }
+
+    @Override
+    public int ascend(int meters) {  
+        altitude = getAltitude() + meters;      
+        System.out.printf("%s flies upward, altitude: %d .%n", getName(), altitude);
+        return altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        altitude = Math.max(getAltitude() - meters, 0); 
+        System.out.printf("%s flies downward, altitude: %d.%n", getName(), altitude);
+        return altitude;
+    }
+
+    @Override
+    public void land() {
+        System.out.println("Hawkeye lands on the ground.");
+        
     }
 }
